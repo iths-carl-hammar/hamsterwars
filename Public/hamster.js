2b6b62a2407de87aa4f99d30c79ddcac462ed031
kalle75 <carl.hammar@gmail.com>
2020-05-12 09:11:02 +0200

    games

--- Public/hamster.js
+++ Public/hamster.js
@@ -14,6 +14,11 @@ router.get('/', async (req, res)=>{
 
 })
 
+router.put('/:id/result',async (req, res)=>{
+  let snapShot = await db.collection('hamsters').where('id', "==", req.params.id).get();
+    console.log(snapShot);
+})
+
 router.collection('hamsters').where('id', '==',req.params.id).get();
 
 module.exports = router;
---
+++ Public/stats.js
@@ -0,0 +1,12 @@
+
+const {db} = require('./../firebase');
+const {Router} = require('express');
+const express = require('express');
+
+
+const stats = {
+    totalsGames: 10,
+    lose: 0
+
+}
+
