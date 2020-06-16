7d28315cca3fa191f3e23d1b28711541fb8b294e
kalle75 <carl.hammar@gmail.com>
2020-06-16 15:14:43 +0200

    my-app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import { useState } from 'react';
 
 import './App.css';
 import Home from './Pages/Home';
@@ -7,6 +8,12 @@ import About from './Pages/About';
 import User from './Pages/User';
 
 function App() {
+  const [hamster, setHamster] = useState(null);
+   const handleClick = () =>{
+   let array =   getBild();
+   setHamster(array);
+   }
+
   return (
     <Router>
     <div className="App">
@@ -39,9 +46,20 @@ function App() {
        
       
       </header>
+      <div>
+        <button onClick={handleClick}>Get hamster</button>
+      </div>
     </div>
     </Router>
   );
+}
+
+async function getBild(){
+  let baseUrl = '/';
+ const response = await fetch('/');
+
+ const hamsterArray = await response.json();
+ return hamsterArray;
 }
 
 export default App;
