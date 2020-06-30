142bbcce4a6cd7de191835d4910bed42f03722aa
kalle75 <carl.hammar@gmail.com>
2020-06-30 12:55:57 +0200

    about

---
+++ kalle-app/src/components/About.js
@@ -0,0 +1,17 @@
+import react, { useState } from 'react';
+
+
+const About = ()=>{
+    const [] = useState();
+
+    const fetch
+
+     return(
+         <div>
+
+         </div>
+     )
+    
+}
+
+export default About;
